fd5308d412dc8bfad1647ecbcff9c0eb9ebf5a77
tareem <tareemabuabah@gmail.com>
2026-07-13 11:52:40 +0300

    Render tasks dynamically using map

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,17 @@
 import TaskItem from "./TaskItem";
 
-export default function TaskList() {
+export default function TaskList({ tasks }) {
   return (
     <div className="task-list">
-      <TaskItem
-        title="Task 1"
-        date="2026-07-07"
-        time="14:57"
-        completed={false}
-      />
-
-      <TaskItem
-        title="Task 2"
-        date="2026-07-06"
-        time="14:58"
-        completed={true}
-      />
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          title={task.title}
+          date={task.date}
+          time={task.time}
+          completed={task.completed}
+        />
+      ))}
     </div>
   );
 }
