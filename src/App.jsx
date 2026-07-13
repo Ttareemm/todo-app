741119000725aebe03077961c096dd4732991913
tareem <tareemabuabah@gmail.com>
2026-07-13 17:56:56 +0300

    Manage tasks using React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
 import TasksSummary from "./components/TasksSummary";
 import TaskList from "./components/TaskList";
-import tasks from "./data/tasks";
+import initialTasks from "./data/tasks";
 
 export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  function addTask(newTask) {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  }
+
+  function toggleTask(id) {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    );
+  }
+
   const completedTasks = tasks.filter((task) => task.completed).length;
 
   return (
@@ -13,14 +30,17 @@ export default function App() {
       <Header />
 
       <section className="tasks-container">
-        <TaskForm />
+        <TaskForm onAddTask={addTask} />
 
         <TasksSummary
           completedTasks={completedTasks}
           totalTasks={tasks.length}
         />
 
-        <TaskList tasks={tasks} />
+        <TaskList
+          tasks={tasks}
+          onToggleTask={toggleTask}
+        />
       </section>
     </main>
   );
