1355ac422d8f46894725e013b68c466e79fd9eb7
tareem <tareemabuabah@gmail.com>
2026-07-13 17:59:45 +0300

    Pass task actions to task items

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,17 @@
 import TaskItem from "./TaskItem";
 
-export default function TaskList({ tasks }) {
+export default function TaskList({ tasks, onToggleTask }) {
   return (
     <div className="task-list">
       {tasks.map((task) => (
         <TaskItem
           key={task.id}
+          id={task.id}
           title={task.title}
           date={task.date}
           time={task.time}
           completed={task.completed}
+          onToggleTask={onToggleTask}
         />
       ))}
     </div>
